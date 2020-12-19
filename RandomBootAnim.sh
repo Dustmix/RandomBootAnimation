1b843492f201e9e4abcb8eab8f3c7806afa56c11
Dustmix <43365173+Dustmix@users.noreply.github.com>
2020-12-19 18:59:13 +0100

    First commit

---
+++ RandomBootAnim.sh
@@ -0,0 +1,34 @@
+#!/system/bin/sh
+
+# Variables
+BOOTANIFOLDER=/sdcard/Bootanimations/
+# Try replacing /system with /system/system/media if this doesn't work
+SYSMEDIAPATH=/system/media
+
+# Remount system
+echo "Remounting system (rw)..."
+mount -o rw,remount /system
+
+# Search Bootanimation folder and setting it
+if [[ -d "${BOOTANIFOLDER}" ]]; then
+  echo "Bootanimation folder found! Selecting a random animation..."
+  selected_zip=`find "${BOOTANIFOLDER}" -type f -iname \*.zip | shuf -n 1`
+  echo "Setting animation... (${selected_zip})"
+  cp ${selected_zip} ${SYSMEDIAPATH}/bootanimation.zip
+else
+  echo "Bootanimation folder not found. Searching original Bootanimation..."
+  if [[ -f "${SYSMEDIAPATH}/bootanimation.zip_original" ]]; then
+    echo "Original Bootanimation found, using that."
+    cp ${SYSMEDIAPATH}/bootanimation.zip_original ${SYSMEDIAPATH}/bootanimation.zip
+  else
+    echo "Original Bootanimation not found. Quitting..."
+    exit 1
+  fi
+fi
+
+# Remount system read-only
+echo "Remounting system (ro) again...."
+mount -o ro,remount /system
+
+echo "Done"
+exit 0
---
+++ readme.md
@@ -0,0 +1,5 @@
+# RandomBootAnimation
+
+This script aims to set a random Bootanimation each time it is started (for example at boot).
+Just put some bootanimations in the "Bootanimations" folder in the root (the "main folder") of the main memory.
+**Note: You have to provvide non flashable zips, they should contain just part.txt and a few folders named something like partXX**
