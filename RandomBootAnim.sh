0ea8d4fe2187f62cd45fcd535d18e58c2c836fa6
Dustmix <43365173+Dustmix@users.noreply.github.com>
2020-12-20 00:22:29 +0100

    Version 2
    Added logging, a message when fixing permissions and more settings (variables), also this release isn't 100% tested, so bewere.

--- RandomBootAnim.sh
+++ RandomBootAnim.sh
@@ -1,37 +1,130 @@
 #!/system/bin/sh
 
+#
+# RandomBootAnim was created by Dustmix
+# Created on: 19/12/2020 (DD/MM/YYYY)
+# Repo: https://github.com/Dustmix/RandomBootAnimation
+#
+
+
 # Variables
 BOOTANIFOLDER=/sdcard/Bootanimations/
+LOGGING=0
+LOGFILE=/sdcard/RBA_log.log
+KEEPOLDLOGS=0
 # Try replacing /system with /system/system/media if this doesn't work
-SYSMEDIAPATH=/system/media
+BOOTANIMATIONZIP=/system/media/bootanimation.zip
 
-# Remount system
-echo "Remounting system (rw)..."
-mount -o rw,remount /system
-
-# Search Bootanimation folder and setting it
-if [[ -d "${BOOTANIFOLDER}" ]]; then
-  echo "Bootanimation folder found! Selecting a random animation..."
-  selected_zip=`find "${BOOTANIFOLDER}" -type f -iname \*.zip | shuf -n 1`
-  echo "Setting animation... (${selected_zip})"
-  cp ${selected_zip} ${SYSMEDIAPATH}/bootanimation.zip
-else
-  echo "Bootanimation folder not found. Searching original Bootanimation..."
-  if [[ -f "${SYSMEDIAPATH}/bootanimation.zip_original" ]]; then
-    echo "Original Bootanimation found, using that."
-    cp ${SYSMEDIAPATH}/bootanimation.zip_original ${SYSMEDIAPATH}/bootanimation.zip
+# Check if logging is enabled and if it is start logging
+if [[ -f ${LOGFILE} ]]; then
+  LOGGING=1
+  if [ "${KEEPOLDLOGS}" -eq 1 ]; then
+    echo "RBA started:" >> ${LOGFILE}
+    date >> ${LOGFILE}
   else
-    echo "Original Bootanimation not found. Quitting..."
-    exit 1
+    echo "RBA started:" > ${LOGFILE}
+    date >> ${LOGFILE}
   fi
 fi
 
+# Remount system
+if [ ${LOGGING} -eq 1 ]; then
+  echo "Remounting system (rw)" >> ${LOGFILE}
+  echo "Remounting system (rw)..."
+  mount -o rw,remount /system
+  echo "System remounted" >> ${LOGFILE}
+else
+  echo "Remounting system (rw)..."
+  mount -o rw,remount /system
+fi
+
+# Backup Bootanimation if a backup doesn't exist
+if [[ ! -f "${BOOTANIMATIONZIP}_original" ]]; then
+  if [ ${LOGGING} -eq 1 ]; then
+    echo "Backup doesn't exist, backing up original animation." >> ${LOGFILE}
+    echo "Backing up original bootanimation..."
+    cp ${BOOTANIMATIONZIP} "${BOOTANIMATIONZIP}_original"
+    echo "bootanimation.zip backed up to /system/media/bootanimation.zip_original" >> ${LOGFILE}
+  else
+    echo "Backing up original bootanimation..."
+    cp ${BOOTANIMATIONZIP} "${BOOTANIMATIONZIP}_original"
+  fi
+else
+  if [ ${LOGGING} -eq 1 ]; then
+      echo "Backup exists, continuing..." >> ${LOGFILE}
+  fi
+fi
+
+# Search Bootanimation folder and setting it
+
+if [ ${LOGGING} -eq 1 ]; then
+  if [[ -d "${BOOTANIFOLDER}" ]]; then
+    echo "Bootanimation folder was found, choosing a random animation" >> ${LOGFILE}
+    echo "Bootanimation folder found! Selecting a random animation..."
+    selected_zip=`find "${BOOTANIFOLDER}" -type f -iname \*.zip | shuf -n 1`
+    echo "Replacing bootanimation.zip" >> ${LOGFILE}
+    echo "Choosen animation: ${selected_zip}" >> ${LOGFILE}
+    echo "Setting animation... (${selected_zip})"
+    cp ${selected_zip} ${BOOTANIMATIONZIP}
+    echo "bootanimation.zip replaced" >> ${LOGFILE}
+  else
+    echo "Bootanimation folder was not found, searching original bootanimation.zip" >> ${LOGFILE}
+    echo "Bootanimation folder not found. Searching original Bootanimation..."
+    if [[ -f "${BOOTANIMATIONZIP}_original" ]]; then
+      echo "Original bootanimation.zip found, setting that as default bootanimation.zip" >> ${LOGFILE}
+      echo "Original Bootanimation found, using that."
+      cp "${BOOTANIMATIONZIP}_original" ${BOOTANIMATIONZIP}
+      echo "bootanimation.zip replaced" >> ${LOGFILE}
+    else
+      echo "Original bootanimation.zip not found. Quitting..." >> ${LOGFILE}
+      echo "Original Bootanimation not found. Quitting..."
+      exit 1
+    fi
+  fi
+else
+  if [[ -d "${BOOTANIFOLDER}" ]]; then
+    echo "Bootanimation folder found! Selecting a random animation..."
+    selected_zip=`find "${BOOTANIFOLDER}" -type f -iname \*.zip | shuf -n 1`
+    echo "Setting animation... (${selected_zip})"
+    cp ${selected_zip} ${BOOTANIMATIONZIP}
+  else
+    echo "Bootanimation folder not found. Searching original Bootanimation..."
+    if [[ -f "${BOOTANIMATIONZIP}_original" ]]; then
+      echo "Original Bootanimation found, using that."
+      cp "${BOOTANIMATIONZIP}_original" ${BOOTANIMATIONZIP}
+    else
+      echo "Original Bootanimation not found. Quitting..."
+      exit 1
+    fi
+  fi
+fi
+
+
+
+
+
 # Fix permissions
-chmod 0644 ${SYSMEDIAPATH}/bootanimation.zip
+if [ ${LOGGING} -eq 1 ]; then
+  echo "Fixing permissions" >> ${LOGFILE}
+  echo "Fixing permissions..."
+  chmod 0644 ${BOOTANIMATIONZIP}
+  echo "Permission fixed" >> ${LOGFILE}
+else
+  echo "Fixing permissions..."
+  chmod 0644 ${BOOTANIMATIONZIP}
+fi
 
 # Remount system read-only
-echo "Remounting system (ro) again...."
-mount -o ro,remount /system
+if [ ${LOGGING} -eq 1 ]; then
+  echo "Remount system (ro)" >> ${LOGFILE}
+  echo "Remounting system (ro) again...."
+  mount -o ro,remount /system
+  echo "System remounted" >> ${LOGFILE}
+else
+  echo "Remounting system (ro) again...."
+  mount -o ro,remount /system
+fi
 
 echo "Done"
+echo "Done" >> ${LOGFILE}
 exit 0
--- readme.md
+++ readme.md
@@ -1,6 +1,33 @@
+
 # RandomBootAnimation
 
 This script aims to set a random Bootanimation each time it is started (for example at boot).
-Just put some bootanimations in the "Bootanimations" folder in the root (the "main folder") of the main memory.
-You must provvide non flashable zips, they should contain just part.txt and a few folders named something like partXX.
-**This script must be executed as root or it will not work so rooting your Android device is required.**
+**Note: This script requires rooting your phone.**
+
+## Disclaimer
+
+I'm not responsible if you damage your device, also I know this is not the best way to script but I'm not a professional.
+
+# Setup
+
+ 1. Create a Bootanimations folder in the root (the "main folder") of the main memory.
+ 2. Put some Bootanimations in that folder
+ 3. Done  
+ 
+ Just start the script ***as root*** and it will do everything else by itself.
+**Note: You must provvide non flashable zips, they should only contain images (in folders like part01...) and desc.txt**
+
+# Logging
+
+You can enable logging by setting the "LOGGING" variable to 1 or creating the RBA_log.log (can be changed) file in the root of your main memory.
+You can also enable "KEEPOLDLOGS" by setting it to 1, it doesn't delete the old logs when the script is started.
+
+# Config
+
+In the Variables section in the script you can change:
+
+ - The Bootanimations folder with the "BOOTANIFOLDER" variable;
+ - Enable (1) / Disable (0) logging via the "LOGGING" variable;
+ - The log file with the "LOGFILE" variable;
+ - Enable (1) / Disable (0) the "archive" of old logs via the "KEEPOLDLOGS" variable;
+ - The Bootanimation.zip path with the "BOOTANIMATIONZIP" variable.
